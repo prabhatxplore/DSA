4962ea582747bf50d8c52845324dd69b10cd8eff
Prabhat BK <prabhatbk4@gmail.com>
2025-04-23 21:40:36 +0545

    added merge sort in c

---
+++ Sorting/merge_sort.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+void merge_sort(int a[], int length);
+void merge_sort_recursion(int a[], int l, int r);
+void merge_sorted_arrays(int a[], int l, int m, int r);
+
+int main()
+
+{
+    int arr[] = {1, 2, 0, 3, 6, 5, 8, 4,5};
+    int len = sizeof(arr) / sizeof(int);
+    merge_sort(arr, len);
+
+    for (int i = 0; i < len; i++)
+    {
+        printf("%d  ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
+void merge_sort(int a[], int length)
+{
+    merge_sort_recursion(a, 0, length - 1);
+}
+void merge_sort_recursion(int a[], int l, int r)
+{
+    if (l < r)
+    {
+        int m = (l + r) / 2;
+        merge_sort_recursion(a, l, m);
+        merge_sort_recursion(a, m + 1, r);
+        merge_sorted_arrays(a, l, m, r);
+    }
+}
+void merge_sorted_arrays(int a[], int l, int m, int r)
+{
+    int left_length = m - l + 1;
+    int right_length = r - m;
+    int temp_left[left_length];
+    int temp_right[right_length];
+
+    int i, j, k;
+    for (int i = 0; i < left_length; i++)
+    {
+        temp_left[i] = a[l + i];
+    }
+    for (int i = 0; i < right_length; i++)
+    {
+        temp_right[i] = a[m + 1 + i];
+    }
+    for (i = 0, j = 0, k = l; k <= r; k++)
+    {
+        if ((i < left_length) && (j >= right_length || temp_left[i] <= temp_right[j]))
+        {
+            a[k] = temp_left[i];
+            i++;
+        }
+        else
+        {
+            a[k] = temp_right[j];
+            j++;
+        }
+    }
+}
